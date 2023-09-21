b23a5fdf80a915734ed22b7a6827c78f9bb6abf4
Marco Luo <mailforrgscid@gmail.com>
2023-09-21 11:28:37 +0100

    enable ping endpoint

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+ruby '3.2.2'
+
+gem 'sinatra', '3.1.0'
+gem 'puma', '6.4.0'
--- app.rb
+++ app.rb
@@ -1,1 +1,6 @@
-# TODO: write Sinatra app
+require 'sinatra'
+
+get '/*' do
+  return [200, 'OK'] if params['splat'][0] == 'ping'
+  redirect to('/ping')
+end
