b203fffc01692d25b86e81a68c301a061fb583d7
Marco Luo <mailforrgscid@gmail.com>
2023-09-21 16:21:52 +0100

    remove unused settings

--- app.rb
+++ app.rb
@@ -4,8 +4,6 @@ require_relative 'lib/config'
 set :environment, Config.env
 set :port, Config.port if Config.port
 
-use Rack::Logger, 'DEBUG'
-
 get '/*' do
   return [200, 'OK'] if params['splat'][0] == 'ping'
   redirect to('/ping')
