0349a9a89645779e9420020808f78cdede7664fa
Marco Luo <mailforrgscid@gmail.com>
2023-09-22 12:28:05 +0100

    remove ping endpoint testing implementations

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ require_relative 'lib/config'
 set :environment, Config.env
 set :port, Config.port if Config.port
 
-get '/*' do
-  return [200, 'OK'] if params['splat'][0] == 'ping'
-  redirect to('/ping')
+get '/ping' do
+  'OK'
 end
