d574ab712ff36d9c0ad55a0ed33606f2f531b3db
Marco Luo <mailforrgscid@gmail.com>
2023-09-22 10:45:07 +0100

    create Project class and relevant empty methods

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,23 @@
-# TODO: write Project class
+class Project
+
+  def verify_credential(credential)
+    # TODO credential verification implementation
+    # return @credential == credential
+  end
+
+  def show_active_instances()
+  # TODO fetch list of active instances
+  # return instance list
+  end
+
+  def start_instance(instance_id)
+    # TODO send some commands to start the instance
+    # return something?
+  end
+
+  def stop_instance(instance_id)
+    #TODO send some commands to stop the instance
+    # return something?
+  end
+
+end
