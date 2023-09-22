3a476150618119057d001ac60b12d46189f77387
Marco Luo <mailforrgscid@gmail.com>
2023-09-22 16:49:33 +0100

    config example modified to fit new implementation

--- etc/config.yml.ex
+++ etc/config.yml.ex
@@ -1,27 +1,17 @@
-# This is the example of the global configuration file.
-# Please remove the ".ex" extension to make it a valid configuration file.
-
-# set the environment the application will be running on.
-#
-# the priority of this option is: global config > ENV['APP_ENV'] > default, which means:
-#  - if the option is enabled here, the system environment variable will be igored, including the APP_ENV='some environment' setting prepend in the CLI command.
-#  - if neither the option is enabled nor the 'APP_ENV' system environment variable is set, a default environment will be used, which is development environment.
-#
-#env: production
-
-# the absolute path to the config files for different environments.
+# This is the example of the configuration file
 # 
-# if the option is not enabled, the paths will be /path/to/flight-control-api/etc/environment-name.yml by default.
-# 
-#development_config_path: C:/CenaCloudStorage/development.yml
-#production_config_path: /path/to/production.yml
+# To make it a valid configuration file, simply copy the file,
+# and remove the ".ex" file extension.
+#
+# To enable an option, modify the value of the option to be enabled,
+# and remove the '# ' comment symbol before it.
 
-# set the port the application listening on.
-#
-# the priority of this option is: app environment config > global config > cli config > default, which means:
-#   - if the port is defined in the config file for the corresponding environment, the option enabled here will be ignored.
-#   - if the option is enabled here, the -p option appended in the CLI command will be ignored when starting the application from the console.
-#   - if the port is specified nowhere, a default port will be used, which generally should be 4567.
-# if 0 is given as the value for this option, the application will listen on a random availabe port.
-#
-#port: 4567
+# The following are the available options for the development environment
+# 
+development:  # Warining! If you want to comment out all the options to disable them, please at least leave this line uncommented.
+#  port: 4567 # The port the server will be listening on
+
+# The following are the available options for the production environment
+# 
+# production:
+#   port: 4567
