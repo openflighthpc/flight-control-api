74e83228b15152aac41c7c256dd756928b70b5d9
Marco Luo <mailforrgscid@gmail.com>
2023-09-25 12:23:13 +0100

    example configuration file updated

--- etc/config.yml.ex
+++ etc/config.yml.ex
@@ -8,10 +8,15 @@
 
 # The following are the available options for the development environment
 # 
-development:  # Warining! If you want to comment out all the options to disable them, please at least leave this line uncommented.
-#  port: 4567 # The port the server will be listening on
+development:    # Warining! If you want to comment out all the options to disable them, please at least leave this line uncommented.
+#   port: 4567  # The port the server will be listening on
 
 # The following are the available options for the production environment
 # 
 # production:
+#   port: 4567
+
+# The following are the available options for the test environment
+# 
+# test:
 #   port: 4567
