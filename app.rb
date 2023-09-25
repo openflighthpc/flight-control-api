f93d2d822f734e9ae82e5fc06a2d674edb4952ff
Marco Luo <mailforrgscid@gmail.com>
2023-09-25 12:35:47 +0100

    prioritize environment variables over the config file

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/config_file'
 
 config_file ENV['CONFIG_PATH'] || 'etc/config.yml'
+set :bind, ENV['BIND'] || settings.bind
+set :port, ENV['PORT'] || settings.port
 
 get '/ping' do
   'OK'
