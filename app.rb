caa2521f76a345b25b988654cf696faed521e27b
Marco Luo <mailforrgscid@gmail.com>
2023-09-25 14:37:54 +0100

    enhance environment variable priority implementation

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'sinatra/config_file'
 
 config_file ENV['CONFIG_PATH'] || 'etc/config.yml'
-set :bind, ENV['BIND'] || settings.bind
-set :port, ENV['PORT'] || settings.port
+set :bind, ENV['BIND'] if ENV['BIND']
+set :port, ENV['PORT'] if ENV['PORT']
 
 get '/ping' do
   'OK'
