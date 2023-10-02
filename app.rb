46c80d6f39e0387c28f4559579dd80fc06284679
Jack Millard <millard64@hotmail.co.uk>
2023-10-02 13:21:15 +0100

    Use lib/config.rb application-wide

--- app.rb
+++ app.rb
@@ -1,25 +1,25 @@
 require 'sinatra'
-require 'sinatra/config_file'
-require "sinatra/custom_logger"
-require "sinatra/namespace"
+require 'sinatra/custom_logger'
 require 'logger'
+
 require_relative './lib/control_api'
 
-config_file ENV['CONFIG_PATH'] || File.join(__dir__, 'etc/config.yml')
+# Set up recognised environments
+set :environments, %w[test production development]
 
 configure do
-  set :bind, ENV['BIND'] if ENV['BIND']
-  set :port, ENV['PORT'] if ENV['PORT']
-  
-  set :backend_config, Config.setup_singleton(provider_path: ENV['PROVIDER_PATH'] || (settings.respond_to?(:provider_path) ? settings.provider_path : nil))
+  set :bind, ENV['BIND'] || Config.fetch(:bind)
+  set :port, ENV['PORT'] || Config.fetch(:port)
 end
 
 # initialize logger
-if settings.respond_to?(:log)
-  ENV['LOG_PATH'] ||= settings.log['path']
-  ENV['LOG_LEVEL'] ||= settings.log['level']
+if Config.fetch(:log)
+  ENV['LOG_PATH'] ||= Config.fetch(:log, :path)
+  ENV['LOG_LEVEL'] ||= Config.fetch(:log, :path)
 end
+
 LOGGER = Logger.new(ENV['LOG_PATH'] || STDOUT)
+
 log_levels = {
   'debug' => Logger::DEBUG,
   'info' => Logger::INFO, 
@@ -27,8 +27,11 @@ log_levels = {
   'error' => Logger::ERROR,
   'fatal' => Logger::FATAL
 }
+
 raise "Invalid log level" if ENV['LOG_LEVEL'] && !log_levels.key?(ENV['LOG_LEVEL'])
+
 LOGGER.level = log_levels[ENV['LOG_LEVEL']] if ENV['LOG_LEVEL']
+
 disable :logging
 use Rack::CommonLogger, LOGGER
 set :logger, LOGGER
@@ -36,18 +39,4 @@ set :logger, LOGGER
 # rest apis
 get '/ping' do
   'OK'
-end
-
-namespace '/providers' do
-  # get providers list
-  get do
-    Provider.all.map(&:to_hash).to_json
-  end
-
-  # get specific provider
-  get '/:id' do
-    id = params['id']
-    return Provider[id].to_hash.to_json if Provider[id]
-    404
-  end
 end
--- lib/control_api/config.rb
+++ lib/control_api/config.rb
@@ -1,19 +1,85 @@
 require 'yaml'
+require 'sinatra/base'
 
 class Config
+  #==============================================================================
+  # Some of the following logic is adapted from Sinatra::SinatraContrib::ConfigFile.
+  # https://github.com/sinatra/sinatra/blob/main/sinatra-contrib/lib/sinatra/config_file.rb
+  #
+  # It is used under the following license:
+  #
+  # The MIT License (MIT)
+  #
+  # Copyright (c) 2007, 2008, 2009 Blake Mizerany
+  # Copyright (c) 2010-2017 Konstantin Haase
+  # Copyright (c) 2015-2017 Zachary Scott
+  #
+  # Permission is hereby granted, free of charge, to any person
+  # obtaining a copy of this software and associated documentation
+  # files (the "Software"), to deal in the Software without
+  # restriction, including without limitation the rights to use,
+  # copy, modify, merge, publish, distribute, sublicense, and/or sell
+  # copies of the Software, and to permit persons to whom the
+  # Software is furnished to do so, subject to the following
+  # conditions:
+  #
+  # The above copyright notice and this permission notice shall be
+  # included in all copies or substantial portions of the Software.
+  #
+  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  # OTHER DEALINGS IN THE SOFTWARE.
+  #==============================================================================
 
-  def self.setup_singleton(...)
-    @singleton = new(...)
-  end
+  class << self
+    def fetch(*keys)
+      config.dig(*keys.map(&:to_s))
+    end
 
-  def self.fetch(key)
-    setup_singleton(provider_path: nil) unless @singleton
-    @singleton.send(key)
-  end
+    def config
+      @config ||= read_from_file('etc/config.yml')
+    end
 
-  attr_reader :provider_path
+    def environment
+      Sinatra::Application.environment
+    end
 
-  def initialize(provider_path:)
-    @provider_path = provider_path || File.join(__dir__, '../../etc/providers')
+    def environments
+      Sinatra::Application.environments
+    end
+
+    def read_from_file(file)
+      document = ERB.new(File.read(file)).result
+      yaml = YAML.respond_to?(:unsafe_load) ? YAML.unsafe_load(document) : YAML.load(document)
+
+      config_for_env(yaml)
+    end
+
+    def config_for_env(hash)
+      return from_environment_key(hash) if environment_keys?(hash)
+
+      hash.each_with_object(Sinatra::IndifferentHash[]) do |(k, v), acc|
+        acc.merge!(k => v)
+
+        if environment_keys?(v)
+          acc.merge!(k => v[environment.to_s]) if v.key?(environment.to_s)
+        else
+          acc.merge!(k => v)
+        end
+      end
+    end
+
+    def from_environment_key(hash)
+      hash[environment.to_s] || hash[environment.to_sym] || {}
+    end
+
+    def environment_keys?(hash)
+      hash.is_a?(Hash) && hash.any? { |k, _| environments.include?(k.to_s) }
+    end
   end
 end
