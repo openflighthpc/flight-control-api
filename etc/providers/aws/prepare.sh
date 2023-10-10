edfdf48950cdaac2480e3b03725f05aacc344105
Stu Franks <stu@stuts.uk>
2023-10-10 16:41:54 +0100

    Add AWS prepare

---
+++ etc/providers/aws/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+set -e
+echo "Preparing..."
+
+mkdir -p $RUN_ENV/aws/cli/bin
+
+temp_dir=$(mktemp -d)
+cd $temp_dir
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" -s
+unzip -qq awscliv2.zip
+./aws/install -i $RUN_ENV/aws/cli/bin/cli/aws_cli -b $RUN_ENV/aws/cli/bin/
+rm -rf $temp_dir
+
+echo "Prepared"
