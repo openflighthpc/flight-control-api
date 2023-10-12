2ed2e52feb85dd03d3c9cc79b078b1cca36bc845
Jack Millard <millard64@hotmail.co.uk>
2023-10-12 11:59:25 +0100

    Amend typos in prepare.sh

--- etc/providers/example-provider/prepare.sh
+++ etc/providers/example-provider/prepare.sh
@@ -1,6 +1,5 @@
 echo "Preparing..."
 mkdir "$RUN_ENV/nodes"
-touch "$RUN_ENV/nodes/login1"
 
 echo "Creating login1..."
 cat <<EOF > "$RUN_ENV/nodes/login1"
@@ -15,7 +14,6 @@ for int in {01..03}
 do
   name="cnode$int"
   echo "Creating $name..."
-  touch "$RUN_ENV/nodes/$name"
 
   cat <<EOF > "$RUN_ENV/nodes/$name"
 ---
@@ -26,9 +24,8 @@ tags:
 EOF
 done
 
-# Download jq for parsing YAML and JSON
-
-echo "wgetting jq"
+# Download yq for parsing YAML and JSON
+echo "wgetting yq"
 mkdir "$RUN_ENV/bin"
 wget https://github.com/mikefarah/yq/releases/download/v4.35.2/yq_linux_amd64 -P "$RUN_ENV/bin"
 chmod +x "$RUN_ENV/bin/yq_linux_amd64"
