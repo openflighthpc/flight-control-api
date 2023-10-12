1493716cdff09f2cc560a0f7fc3bf6dee8754a12
Jack Millard <millard64@hotmail.co.uk>
2023-10-12 12:17:11 +0100

    Update example on/off scripts to modify new dummy data

--- etc/providers/example-provider/actions/stop_instance.sh
+++ etc/providers/example-provider/actions/stop_instance.sh
@@ -1,2 +1,3 @@
 # Stop the given instance. Exit status 0 denotes success.
-echo 'Stopped instance'
+echo "Stopping $INSTANCE_ID"
+"$RUN_ENV/bin/yq_linux_amd64" -i '.state = "off"' "nodes/$INSTANCE_ID"
