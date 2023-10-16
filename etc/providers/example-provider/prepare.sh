43a79fff6bb477ae76e0296dc2232cf5f47fd2f1
Jack Millard <millard64@hotmail.co.uk>
2023-10-16 10:37:31 +0100

    Update example prepare script to create instance types

--- etc/providers/example-provider/prepare.sh
+++ etc/providers/example-provider/prepare.sh
@@ -1,8 +1,38 @@
 echo "Preparing..."
+
+# Set up instance types and costs
+echo "Creating instance types"
+mkdir "$RUN_ENV/instance_types"
+cat <<EOF > "$RUN_ENV/instance_types/compute_small.yaml"
+---
+name: compute_small
+cost_per_hour: 5
+EOF
+
+cat <<EOF > "$RUN_ENV/instance_types/compute_large.yaml"
+---
+name: compute_large
+cost_per_hour: 10
+EOF
+
+cat <<EOF > "$RUN_ENV/instance_types/login.yaml"
+---
+name: login
+cost_per_hour: 5
+EOF
+
+cat <<EOF > "$RUN_ENV/instance_types/gpu.yaml"
+---
+name: gpu
+cost_per_hour: 20
+EOF
+
+
+# Set up login node
 mkdir "$RUN_ENV/nodes"
 
 echo "Creating login1..."
-cat <<EOF > "$RUN_ENV/nodes/login1"
+cat <<EOF > "$RUN_ENV/nodes/login1.yaml"
 ---
 name: login1
 state: on
@@ -10,12 +40,13 @@ tags:
   - type: login
 EOF
 
+# Set up compute nodes
 for int in {01..03}
 do
   name="cnode$int"
   echo "Creating $name..."
 
-  cat <<EOF > "$RUN_ENV/nodes/$name"
+  cat <<EOF > "$RUN_ENV/nodes/$name.yaml"
 ---
 name: $name
 state: on
