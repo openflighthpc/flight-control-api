33f244c31e0ea07c612daffc610d5c3198456dfc
Jack Millard <millard64@hotmail.co.uk>
2023-10-18 14:43:45 +0100

    Move node creation login to authorise credentials script

--- etc/providers/example-provider/actions/authorise_credentials.sh
+++ etc/providers/example-provider/actions/authorise_credentials.sh
@@ -1,2 +1,40 @@
 # This command will authorise credentials. Exit status 0 indicates correct credentials, any other exit status indicates otherwise.
+
+# We also use this file to set up a fake backend 'project'.
+
+project="$RUN_ENV/projects/$PROJECT_NAME"
+if [ -d "$project" ]
+then
+  echo 'Credentials correct'
+  exit 0
+fi
+
+echo "Creating project '$PROJECT_NAME'"
+
+nodes_dir="$project/nodes"
+mkdir "$nodes_dir"
+
+echo "Creating login1.yaml..."
+cat <<EOF > "$nodes_dir/login1.yaml"
+---
+name: login1
+state: on
+tags:
+  - type: login
+EOF
+
+for int in {01..03}
+do
+  name="cnode$int.yaml"
+  echo "Creating $name..."
+
+  cat <<EOF > "$nodes_dir/$name"
+---
+name: $name
+state: on
+tags:
+    - type: compute
+EOF
+done
+
 echo 'Credentials correct'
