ad6e9e6c86c7527608f1cfd643e747ff17b964da
Jack Millard <millard64@hotmail.co.uk>
2023-10-19 14:12:12 +0100

    Provide full filepath to example project directory in action scripts
    Provide full filepath to example project directory in action scripts

--- etc/providers/example-provider/actions/list_instances.sh
+++ etc/providers/example-provider/actions/list_instances.sh
@@ -1,2 +1,2 @@
 # List instances for a given project, along with their tags. Output is JSON data.
-"$RUN_ENV/bin/yq_linux_amd64" eval-all '[.]' nodes/* -o=json
+"$RUN_ENV/bin/yq_linux_amd64" eval-all '[.]' "$RUN_ENV/projects/$PROJECT_NAME/nodes"/* -o=json
