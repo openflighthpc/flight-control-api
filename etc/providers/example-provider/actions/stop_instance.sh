74fe635640dcef7a42550351e530ddc741872ce8
Jack Millard <millard64@hotmail.co.uk>
2023-10-24 17:30:34 +0100

    ~Started~ Stopped typo

--- etc/providers/example-provider/actions/stop_instance.sh
+++ etc/providers/example-provider/actions/stop_instance.sh
@@ -1,4 +1,4 @@
 # Stop the given instance. Exit status 0 denotes success.
 echo "Stopping $INSTANCE_ID"
 "$RUN_ENV/bin/yq_linux_amd64" -i '.state = "off"' "$RUN_ENV/projects/$PROJECT_NAME/nodes/$INSTANCE_ID.yaml"
-echo "Started $INSTANCE_ID"
+echo "Stopped $INSTANCE_ID"
