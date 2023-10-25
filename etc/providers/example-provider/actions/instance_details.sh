a186d2a66416bce08652da284f2344571e3ac6a6
Marco Luo <mailforrgscid@gmail.com>
2023-10-25 14:59:38 +0100

    fix instance details script

--- etc/providers/example-provider/actions/instance_details.sh
+++ etc/providers/example-provider/actions/instance_details.sh
@@ -1,3 +1,2 @@
 # List instances for a given project, along with their tags. Output is JSON data.
-result = $("$RUN_ENV/bin/yq_linux_amd64" eval-all '[.]' "$RUN_ENV/projects/$PROJECT_NAME/detailed_nodes"/*)
-done
+"$RUN_ENV/bin/yq_linux_amd64" eval-all '[.]' "$RUN_ENV/projects/$PROJECT_NAME/detailed_nodes"/* -o=json
