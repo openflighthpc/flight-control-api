88f86d529e7afc0e2e2183c2031bbdf03aca1e2b
Jack Millard <millard64@hotmail.co.uk>
2023-10-26 11:31:51 +0100

    Add shebang to bash scripts for example provider

--- etc/providers/example-provider/actions/list_instances.sh
+++ etc/providers/example-provider/actions/list_instances.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 # List instances for a given project, along with their tags. Output is JSON data.
 "$RUN_ENV/bin/yq_linux_amd64" eval-all '[.]' "$RUN_ENV/projects/$PROJECT_NAME/nodes"/* -o=json
--- etc/providers/example-provider/actions/start_instance.sh
+++ etc/providers/example-provider/actions/start_instance.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Start the given instance. Exit status 0 denotes success.
 echo "Starting $INSTANCE_ID"
 "$RUN_ENV/bin/yq_linux_amd64" -i '.state = "on"' "$RUN_ENV/projects/$PROJECT_NAME/nodes/$INSTANCE_ID.yaml"
