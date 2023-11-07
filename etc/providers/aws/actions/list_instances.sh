5890fe6db2b66cfca02880d81b232bc8293bba79
Stu Franks <stu@stuts.uk>
2023-11-07 16:36:24 +0000

    Remove tags from output if not set

--- etc/providers/aws/actions/list_instances.sh
+++ etc/providers/aws/actions/list_instances.sh
@@ -7,8 +7,13 @@ if [[ ! -z ${SCOPE} ]] ; then
 fi
 
 # Get instance list
-LIST=$(aws ec2 describe-instances $FILTERS --query 'Reservations[].Instances[].{instance_id: InstanceId, model: InstanceType, state: State.Name, tags: {project: Tags[?Key==`project`]|[0].Value, type: Tags[?Key==`type`]|[0].Value}}' |tr -d '\n ')
+LIST=$(aws ec2 describe-instances $FILTERS --query 'Reservations[].Instances[].{instance_id: InstanceId, model: InstanceType, state: State.Name, tags: {project: Tags[?Key==`project`]|[0].Value, type: Tags[?Key==`type`]|[0].Value}}')
 
 # Add region to list
-echo "$LIST" |jq ".[] += { \"region\": \"$AWS_REGION\" }"
+LIST=$(echo "$LIST" |jq ".[] += { \"region\": \"$AWS_REGION\" }")
 
+# Remove tags if not present (AWS returns the values for these as null)
+LIST=$(echo "$LIST" |jq 'del(.. | select(. == null) )')
+
+# Return 1 line response
+echo "$LIST"  |tr -d '\n '
